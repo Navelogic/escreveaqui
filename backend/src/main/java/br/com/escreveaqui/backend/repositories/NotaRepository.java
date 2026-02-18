d6d292c15f5dec3318f0a701808b35b4cac0f195
Arian Weslley <weslleysantoszbd@gmail.com>
2026-02-18 07:54:18 -0300

    Repo para Repository

---
+++ backend/src/main/java/br/com/escreveaqui/backend/repositories/NotaRepository.java
@@ -0,0 +1,29 @@
+package br.com.escreveaqui.backend.repositories;
+
+import br.com.escreveaqui.backend.models.Nota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface NotaRepository extends JpaRepository<Nota, UUID> {
+
+    @Transactional(readOnly = true)
+    Optional<Nota> findBySlug(String slug);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Nota n WHERE n.updatedAt < :dateTime")
+    int deleteOldNotes(@Param("dateTime") OffsetDateTime dateTime);
+
+    boolean existsBySlug(String slug);
+
+
+}
